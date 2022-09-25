2c79b5b96f8f8782ded64eb8be290a97555d4968
italaibek <talaibek.iskakov@gmail.com>
2022-09-25 17:08:28 +0600

    creating an object "personalMovieDB"

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
