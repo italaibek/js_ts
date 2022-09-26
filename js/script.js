dffa1e685b4a56ecee0388f0fe6edb0ebcb42c07
italaibek <talaibek.iskakov@gmail.com>
2022-09-26 17:49:02 +0600

    adding different conditions to personalMovieDB.count

--- js/script.js
+++ js/script.js
@@ -15,13 +15,20 @@ for (let i = 0; i < 2; i++) {
     if (a != null && b != null && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
         console.log('Done');
-    }
-    else{
+    } else {
         console.log('Error');
         i--;
     }
-
-
-
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("ВЫ классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
 console.log(personalMovieDB);
