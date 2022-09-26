49441501bdffda4217f4f4d50ae8ed39397a3a74
italaibek <talaibek.iskakov@gmail.com>
2022-09-26 11:25:02 +0600

    changing code using a loop

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?","");
-const b = prompt("На сколько оцените его?");
-const c = prompt("Один из последних просмотренных фильмов?","");
-const d = prompt("На сколько оцените его?");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?","");
+    const b = prompt("На сколько оцените его?");
+    personalMovieDB.movies[a] = b;
+    
+}
+console.log(personalMovieDB);
