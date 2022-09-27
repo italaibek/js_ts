b7726852df447a734cbc591b399d74355ca2e0ed
italaibek <talaibek.iskakov@gmail.com>
2022-09-27 11:10:11 +0600

    creating the function "detectPersonalLevel"

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
         const a = prompt("Один из последних просмотренных фильмов?", "");
         const b = prompt("На сколько оцените его?");
-    
+
         if (a != null && b != null && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
             console.log('Done');
@@ -37,14 +37,18 @@ function rememberMyFilms() {
 
 rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("ВЫ классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("ВЫ классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
 }
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
