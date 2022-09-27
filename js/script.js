d19e2fa650386d52ff95d7df528a391e49bd831f
italaibek <talaibek.iskakov@gmail.com>
2022-09-27 11:08:10 +0600

    creating the function "rememberMyFilms"

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ function start() {
 
 start();
 
+
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -18,18 +20,22 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", "");
-    const b = prompt("На сколько оцените его?");
-
-    if (a != null && b != null && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done');
-    } else {
-        console.log('Error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов?", "");
+        const b = prompt("На сколько оцените его?");
+    
+        if (a != null && b != null && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+        } else {
+            console.log('Error');
+            i--;
+        }
     }
 }
+
+rememberMyFilms();
 
 if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
